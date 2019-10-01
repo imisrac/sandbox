8ea2865300607e1fb3ed9b19731f55481f4f45a8
Imre Németh <imre.nemeth@ericsson.com>
2019-10-01 17:46:34 +0200

    Chuck Norris first version

---
+++ src/main/java/codingame/ChuckNorris.java
@@ -0,0 +1,50 @@
+package codingame;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class ChuckNorris {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		String message = in.nextLine();
+		System.err.println(message);
+
+		String binaryMessage = message.chars()
+				.boxed()
+				.map(Integer::toBinaryString)
+				.map(ChuckNorris::getWithPadding)
+				.collect(Collectors.joining());
+
+		System.err.println(binaryMessage);
+
+		StringBuilder output = new StringBuilder();
+		for (int i = 0; i < binaryMessage.length(); i++) {
+			int count = 1;
+			String binaryChar = binaryMessage.split("")[i];
+			while (i < binaryMessage.length() - 1 && binaryChar.equals(binaryMessage.split("")[i + 1])) {
+				count++;
+				i++;
+			}
+			if (binaryChar.equals("1")) {
+				output.append("0 ");
+			} else {
+				output.append("00 ");
+			}
+			output.append(IntStream.generate(() -> 0).limit(count).mapToObj(String::valueOf).collect(Collectors.joining())).append(" ");
+		}
+
+		System.out.println(output.toString().trim());
+	}
+
+	private static String getWithPadding(String binaryChar) {
+		if (binaryChar.length() < 7) {
+			binaryChar =
+					IntStream.generate(() -> 0).limit(7 - binaryChar.length()).mapToObj(String::valueOf).collect(Collectors.joining())
+							+ binaryChar;
+		}
+		return binaryChar;
+	}
+}
